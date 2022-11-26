62ec2d984a5fc2026eb536fca21aa4b332c3bc1d
Kuan Tsai <ktsai11486@gmail.com>
2022-11-26 23:32:20 +0800

    修改

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,19 +9,22 @@ public class Solution
 {
     public bool IsPalindrome(string s)
     {
+        
+        if(s.Length== 1 || s == " ") return true;
+
         bool result = true;
-        string after = "";
-        s = s.ToLower();
+        List<char> after = new List<char>();
+        
         foreach (var item in s)
-        {
-            if ((item >= 97 && item <= 122) || (item >= 48 && item <= 57))
+        {            
+            if (char.IsLetterOrDigit(item))
             {
-                after += item;
+                after.Add( char.ToLower(item));
             }
         }
-        for (int i = 0; i < after.Length/2; i++)
+        for (int i = 0; i < after.Count()/2; i++)
         {
-            if (after[i] != after[after.Length - i - 1])
+            if (after[i] != after[after.Count() - i - 1])
             {
                 result = false;
                 break;
